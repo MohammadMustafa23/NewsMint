753368187de6b4364fb78e247ddc18acd511acb1
MohammadMustafa23 <ff774823@gmail.com>
2026-09-07 20:57:59 +0530

    Fixing time Out error On Login

--- Backend/src/config/mail.js
+++ Backend/src/config/mail.js
@@ -12,9 +12,9 @@ const transporter = nodemailer.createTransport({
     pass: SMTP_PASS,
   },
 
-  connectionTimeout: 10000,
-  greetingTimeout: 10000,
-  socketTimeout: 10000,
+  connectionTimeout: 30000,
+  greetingTimeout: 30000,
+  socketTimeout: 30000,
 });
 
 export default transporter;
